1cd083272be47b70ada88f1e076706f37c1c3993
Aleex87 <alessandrodanteabbate@gmail.com>
2025-11-17 11:24:12 +0100

    uset ALTER but from the terminal because there is a bug and created a new column learnt in the database

---
+++ code_alongs/06_crud/sql/alter.sql
@@ -0,0 +1,12 @@
+--that we have to run in the terminal 
+-- first we have to .quit the project 
+-- then we have to run only duckdb data/gloosery.duckdb
+-- we can run this comand
+
+ALTER TABLE database_schema.duckdb_table
+ADD COLUMN learnt BOOLEAN DEFAULT FALSE;
+
+-- it will desapir the fike . wal
+-- then we can .quit from the duckdb data/glossary.duckdb
+
+-- BASICLLY RUN FROMM THE TERMINALE 
